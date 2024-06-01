4f463ec4a9f1639c2b0283f2270042d3d1608032
Alex1and2I <156155179+Alex1and2I@users.noreply.github.com>
2024-06-01 13:21:01 +0800

    Add files via upload

---
+++ download.php
@@ -0,0 +1,9 @@
+$file = '</dewu.apk>'
+
+header("Cache-Control: public");
+header("Content-Description: File Transfer");
+header("Content-Disposition: attachment; filename=$file");
+header("Content-Type: application/zip");
+header("Content-Transfer-Encoding: binary");
+
+readfile($file);
